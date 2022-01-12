c0baa665f1073eddfdfbd46c57f8af95f4a17fbf
Jas Palermo <jasmes@gmail.com>
2022-01-12 13:49:57 -0500

    Initial commit

---
+++ algebra.ts
@@ -0,0 +1,41 @@
+const equationFull: string = "2x-5=7";
+
+
+function splitEquation(equation: string): string[] {
+    let inputEquationLeft: string = equationFull.split("=")[0];
+    let inputEquationRight: string = equationFull.split("=")[1];
+    return [inputEquationLeft, inputEquationRight];
+}
+
+
+let equationLeft: string = splitEquation(equationFull)[0];
+let equationRight: string = splitEquation(equationFull)[1];
+
+console.log(equationLeft, "\n", equationRight);
+
+function splitLeft(equationLeft: string): string[] {
+    console.log(equationLeft.split(""));
+    return equationLeft.split("");
+}
+
+let arrayOfLeft: string[] = splitLeft(equationLeft);
+
+// function getCoefficientX(input: string[]): number {
+//     let coefficientX: number = parseInt(input[0]);
+//     console.log(coefficientX);
+//     return coefficientX;
+// }
+
+let vargen: number[] = [];
+let numbers: number[] = new Array(100);
+for (let i: number = 0; i < numbers.length; i++) {
+    numbers[i] = i;
+}
+console.log(numbers);
+//console.log(arrayOfLeft.length)
+
+for (let i = 0; i < arrayOfLeft.length; i++) {
+    vargen[i] = parseInt(arrayOfLeft[i]);
+    console.log(arrayOfLeft[i], "SEXXXX");
+    console.log(vargen, "KATYYYY");
+}
---
+++ varGenerator.ts
@@ -0,0 +1,12 @@
+let varSub = "varIter";
+let varMain = {};
+varMain[varSub] = 123;
+console.log(varMain);
+console.log(varMain.varIter);
+
+
+//
+// var myVariable = "coco";
+// window[myVariable] = "riko";
+//
+// console.log(coco); // display : riko
